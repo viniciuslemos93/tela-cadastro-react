b6d509a49c966a423b6cd465989d2a9f223c7bd8
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-02 13:44:21 -0300

    style: posicionado elementos do formulário

--- src/components/FormularioCadastro/index.js
+++ src/components/FormularioCadastro/index.js
@@ -4,34 +4,41 @@ function Formulario () {
 
     return (
         <div>
-            <div class="lineInput">
-                <label for="nome">Nome </label>
-                <input type="text" id="nome" />
-            </div>
-            <div class="lineInput">
-                <label for="dataNasc">Data Nascimento </label>
-                <input type="date" id="dataNasc" />
-            </div>
-            <div class="lineInput">
-                <label for="cpf">CPF </label>
-                <input type="text" id="cpf" />
-            </div>
-            <div class="lineInput">
-                <label for="genero">Gênero </label>
-                <input type="" id="genero" />
-            </div>
-            <div class="lineInput">
-                <label for="veiculo">Veículo </label>
-                <input type="text" id="veiculo" />
-            </div>
-            <div class="lineInput">
-                <label for="placa">Placa </label>
-                <input type="text" id="placa" />
-            </div>
-            <div class="lineInput">
-                <label for="anoModelo">Ano Modelo: </label>
-                <input type="text" id="anoModelo" />
-            </div>
+            <form id='form-cadastro'>
+                <div className='container-usuario'>
+                    <div class="lineInput">
+                        <label for="nome">Nome </label>
+                        <input type="text" id="nome" />
+                    </div>
+                    <div class="lineInput">
+                        <label for="dataNasc">Data Nascimento </label>
+                        <input type="date" id="dataNasc" />
+                    </div>
+                    <div class="lineInput">
+                        <label for="cpf">CPF </label>
+                        <input type="text" id="cpf" />
+                    </div>
+                    <div class="lineInput">
+                        <label for="genero">Gênero </label>
+                        <input type="" id="genero" />
+                    </div>
+                </div>
+                
+                <div className='container-veiculo'>
+                    <div class="lineInput">
+                        <label for="veiculo">Veículo </label>
+                        <input type="text" id="veiculo" />
+                    </div>
+                    <div class="lineInput">
+                        <label for="placa">Placa </label>
+                        <input type="text" id="placa" />
+                    </div>
+                    <div class="lineInput">
+                        <label for="anoModelo">Ano Modelo: </label>
+                        <input type="text" id="anoModelo" />
+                    </div>
+                    </div>
+            </form>
         </div>
     )
 }
